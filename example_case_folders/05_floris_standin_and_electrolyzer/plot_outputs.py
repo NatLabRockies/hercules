a9a752d207f4c2b31961d8bf802a0ad9f69d1f52
Genevieve Starke <genevieve.starke@nrel.gov>
2025-06-05 12:09:09 -0400

    Fix ruff formatting

--- example_case_folders/05_floris_standin_and_electrolyzer/plot_outputs.py
+++ example_case_folders/05_floris_standin_and_electrolyzer/plot_outputs.py
@@ -1,8 +1,8 @@
 # Plot the outputs of the simulation
 
 import matplotlib.pyplot as plt
-import pandas as pd
 import numpy as np
+import pandas as pd
 
 # Read the Hercules output file
 df = pd.read_csv("hercules_output_control.csv", index_col=False)
