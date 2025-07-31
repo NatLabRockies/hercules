583b9f2abafeb0023d49e6e80c6fcd49bcffa6e2
paulf81 <paul.fleming@nrel.gov>
2025-07-31 09:36:03 -0700

    update plot ouputs

--- examples/03_wind_and_solar/plot_outputs.py
+++ examples/03_wind_and_solar/plot_outputs.py
@@ -1,38 +1,50 @@
-# Plot the outputs of the simulation
+# Plot the outputs of the simulation for the wind and solar example
 
 import matplotlib.pyplot as plt
 import pandas as pd
+from hercules.utilities import load_h_dict_from_text
 
 # Read the Hercules output file
 df = pd.read_csv("outputs/hercules_output.csv", index_col=False)
 
+# Read in the meta data file
+h_dict = load_h_dict_from_text("outputs/h_dict.echo")
 
-fig, axarr = plt.subplots()  # 2, 1, sharex=True)
 
-# Plot the wind farm power
-ax = axarr
-ax.plot(
+fig, ax = plt.subplots()
+
+# Plot the hybrid plant power
+
+ax.fill_between(
+    df["time"],
+    0,
+    df["wind_farm.power"],
+    label="Wind Power",
+    color="b",
+    alpha=0.5,
+)
+ax.fill_between(
     df["time"],
     df["wind_farm.power"],
-    label="Wind Farm",
+    df["wind_farm.power"] + df["solar_farm.power"],
+    label="Solar Power",
+    color="orange",
+    alpha=0.5,
 )
-
 ax.plot(
     df["time"],
-    df["solar_farm.power"],
-    label="Solar PV",
-)
-
-ax.plot(
-    df["time"],
-    df["plant.power"],
-    label="Total",
+    df["wind_farm.power"] + df["solar_farm.power"],
+    label="Hybrid Plant",
     color="k",
+)
+ax.axhline(
+    h_dict["plant"]["interconnect_limit"], color="r", linestyle="--", label="Interconnect Limit"
 )
 
 ax.grid(True)
 ax.legend()
 ax.set_xlabel("Time [s]")
 ax.set_ylabel("Power [kW]")
+
 
 plt.show()
