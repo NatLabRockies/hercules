744567c24ba440881eb6c126e2ea2ad61301425b
misi9170 <michael.sinner@nrel.gov>
2025-11-06 13:58:13 -0700

    Update example

--- examples/grid/process_results.py
+++ examples/grid/process_results.py
@@ -6,7 +6,9 @@
 # https://nrel.github.io/hercules/gridstatus_download.html#combining-real-time-and-day-ahead-data-for-whoc
 
 import pandas as pd
-from hercules.grid.grid_utilities import generate_locational_marginal_price_dataframe
+from hercules.grid.grid_utilities import (
+    generate_locational_marginal_price_dataframe_from_gridstatus,
+)
 
 # Read the real time and day ahead data
 df_rt = pd.read_feather("gs_spp_lmp_real_time_5_min_2024_01_01_OKGE_FRONTIER.ftr")
@@ -20,7 +22,12 @@ print(df_da.head(10))
 print("--------------------------------")
 
 # Process the data
-df = generate_locational_marginal_price_dataframe(df_da, df_rt)
+df = generate_locational_marginal_price_dataframe_from_gridstatus(
+    df_da,
+    df_rt,
+    day_ahead_market_name="DAY_AHEAD_HOURLY",
+    real_time_market_name="REAL_TIME_5_MIN",
+)
 
 # Print the first 10 rows of the resultant dataframe
 print("First 10 rows of resultant dataframe:")
