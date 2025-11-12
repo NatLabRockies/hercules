8e4b137d9e1a95aef50ac49ec563ea3822d05232
Genevieve Starke <genevieve.starke@nrel.gov>
2025-11-12 18:04:26 -0500

    Update with documentation

--- hercules/plant_components/electrolyzer_plant.py
+++ hercules/plant_components/electrolyzer_plant.py
@@ -19,22 +19,73 @@ class ElectrolyzerPlant(ComponentBase):
         Args:
             h_dict (dict): Dictionary containing simulation parameters including:
             - general: General simulation parameters.
+            - initial_conditions: Initial conditions for the simulation including:
+                - power_available_kw: Initial power available to the electrolyzer [kW]
             - electrolyzer: Electrolyzer plant specific parameters including:
-                - initialize: Whether to initialize the electrolyzer.
+                - initialize: boolean. Whether to initialize the electrolyzer.
                 - initial_power_kW: Initial power input to the electrolyzer [kW].
+                - supervisor:
+                    - system_rating_MW: Total system rating in MW.
+                    - n_stacks: Number of electrolyzer stacks in the plant.
                 - stack: Electrolyzer stack parameters including:
-                    - cell_area: Area of individual cells in the stack [cm^2].
+                    - cell_type: Type of electrolyzer cell (e.g., PEM, Alkaline).
                     - max_current: Maximum current of the stack [A].
                     - temperature: Stack operating temperature [degC].
                     - n_cells: Number of cells per stack.
                     - min_power: Minimum power for electrolyzer operation [kW].
                     - stack_rating_kW: Stack rated power [kW].
-                    - include_degradation_penalty: Whether to include degradation penalty.
+                    - include_degradation_penalty: *Optional* boolean, Whether to include 
+                        degradation penalty.
+                    - hydrogen_degradation_penalty: *Optional* boolean, wether degradation is 
+                        applied to hydrogen (True) or power (False)
+                cell_params: Electrolyzer cell parameters including:
+                    - cell_area: Area of individual cells in the stack [cm^2].
+                    - turndown_ratio: Minimum turndown ratio for stack operation [between 0 and 1].
+                    - max current_density: Maximum current density [A/cm^2].
+                    - p_anode: Anode operating pressure [bar].
+                    - p_cathode: Cathode operating pressure [bar].
+                    - alpha_a: anode charge transfer coefficient.
+                    - alpha_c: cathode charge transfer coefficient.
+                    - i_0_a: anode exchange current density [A/cm^2].
+                    - i_0_c: cathode exchange current density [A/cm^2].
+                    - e_m: membrane thickness [cm].
+                    - R_ohmic_elec: electrolyte resistance [A*cm^2].
+                    - f_1: faradaic coefficien [mA^2/cm^4].
+                    - f_2: faradaic coefficien [mA^2/cm^4].
+                degradation: Electrolyzer degradation parameters including:
+                    - eol_eff_percent_loss: End of life efficiency percent loss [%].
+                    - PEM_params or ALK_params: Degradation parameters specific to PEM or Alkaline
+                         cells:
+                        - rate_steady: Rate of voltage degradation under steady operation alone
+                        - rate_fatigue: Rate of voltage degradation under variable operation alone 
+                        - rate_onoff: Rate of voltage degradation per on/off cycle
                 - controller: Electrolyzer control parameters including:
-                    - n_stacks: Number of electrolyzer stacks in the plant.
                     - control_type: Controller type for electrolyzer plant operation.
-                - costs: Cost parameters for the electrolyzer plant.
-            - initial_conditions: Initial conditions for the simulation.
+                - costs: *Optional* Cost parameters for the electrolyzer plant including:
+                    - plant_params:
+                        - plant_life: integer, Plant life in years
+                        - pem_location: Location of the PEM electrolyzer. Options are 
+                            [onshore, offshore, in-turbine]
+                        - grid_connected: boolean, Whether the plant is connected to the grid or not
+                    - feedstock: Parameters related to the feedstock including:
+                        - water_feedstock_cost: Cost of water per kg of water
+                        - water_per_kgH2: Amount of water required per kg of hydrogen produced
+                    - opex: Operational expenditure parameters including:
+                        - var_OM: Variable operation and maintenance cost per kW 
+                        - fixed_OM: Fixed operation and maintenance cost per kW-year
+                    - stack_replacement: Parameters related to stack replacement costs including:
+                        - d_eol: End of life cell voltage value [V]
+                        - stack_replacement_percent: Stack replacement cost as a percentage of CapEx
+                            [0,1]
+                    - capex: Capital expenditure parameters including:
+                        - capex_learning_rate: Capital expenditure learning rate.
+                        - ref_cost_bop: Reference cost of balance of plant per kW.
+                        - ref_size_bop: Reference size of balance of plant in kW.
+                        - ref_cost_pem: Reference cost of PEM electrolyzer stack per kW.
+                        - ref_size_pem: Reference size of PEM electrolyzer stack in kW.
+                    - finances: Financial parameters including:
+                        - discount_rate: Discount rate for financial calculations [%].
+                        - install_factor: Installation factor for capital expenditure [0,1].
         """
 
         # Store the name of this component
