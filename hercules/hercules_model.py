6a76d79af05ec27c615decbc682f7d784f303dbe
paulf81 <paul.fleming@nrel.gov>
2026-02-20 14:17:13 -0700

    test change

--- hercules/hercules_model.py
+++ hercules/hercules_model.py
@@ -488,6 +488,7 @@ class HerculesModel:
             external_signals_all = self.external_signals_all
             h_dict = self.h_dict
 
+
             # Set current time and run simulation through steps
             self.time = self.starttime
             last_progress_update = 0
